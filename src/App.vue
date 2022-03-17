40db48e248c0c843d7aa950117a25a0c1d2ae4dd
Signs Madueke <signsmadueke1@gmail.com>
2022-03-18 00:04:31 +0100

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+	<div id="app">
+		<div class="header">
+			<form action="#" class="search">
+				<input
+					class="form-control"
+					type="search"
+					placeholder="Search for a lesson"
+					aria-label="Search"
+					v-on:keyup="searchLessons"
+					v-model="searchTxt"
+				/>
+			</form>
+
+			<button
+				class="cart-btn"
+				v-bind:disabled="cartItems.length <= 0"
+				v-on:click="updateShowProduct"
+			>
+				Cart&nbsp;&nbsp;&nbsp;
+				<span class="fas fa-cart-plus"></span>
+				{{ cartCount() }}
+			</button>
+		</div>
+
+		<div v-if="showProduct" id="product">
+			<Lessons
+				:lessons="lessons"
+				v-on:addLesson="addLessonToCart"
+			></Lessons>
+		</div>
+
+		<section v-else id="check-out">
+			<Checkout
+				:cartItems="cartItems"
+				v-on:removeLesson="removeLessonFromCart"
+				v-on:checkOut="checkOut"
+			></Checkout>
+		</section>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "App",
+};
+</script>
